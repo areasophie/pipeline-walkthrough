952c477382abfd9b58997a467075835fc92de681
Masatoshi Hayashi <m@literalice.com>
2020-09-01 22:22:50 +0900

    firt build stage commit

---
+++ stages/build/pipeline-sample-app/app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import *
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+app.config['APP_VERSION'] = 'SNAPSHOT' if os.getenv('APP_VERSION') is None else os.getenv('APP_VERSION')
+app.config['FEATURE_A'] = os.getenv('FEATURE_A')
+
+@app.route('/')
+def index():
+  features = []
+
+  if app.config['FEATURE_A'] is not None:
+    features.append('A')
+
+  return render_template("index.html", features=features, version=app.config['APP_VERSION'])
+
+@app.route('/health')
+def health():
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run()
