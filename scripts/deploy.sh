1921d1a117a47276572fef936c14c7c21017bd69
Masatoshi Hayashi <m@literalice.com>
2020-09-07 17:52:55 +0900

    fixes services

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,8 @@ kubectl apply -f stages/test-alpha/application.yaml
 ## Gamma
 
 kubectl apply -f stages/test-gamma/namespace.yaml
+
+helm upgrade -i flagger-loadtester flagger/loadtester --namespace=pipeline-gamma --set appmesh.enabled=true --set "appmesh.backends[0]=sample-app" --set "appmesh.backends[1]=sample-app-canary"
+
 kubectl apply -f stages/test-gamma/application.yaml
 
